49b183a77782fd704d2c2bba6e6e7dfc2f18c62c
Cindy Chang <cindyisachang@gmail.com>
2024-02-01 11:51:12 +0100

    mods to build

---
+++ index.js
@@ -0,0 +1,7 @@
+const color = require('./color.json');
+const typography = require('./typography.json');
+
+module.exports = {
+  color,
+  typography
+};
--- src/color.js
+++
@@ -1,3 +0,0 @@
-export default {
-  blue: '#0077CC'
-};
